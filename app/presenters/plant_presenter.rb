b098c2905b3c64d066fd27e01f17706686080c9c
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-04-09 17:52:01 +0200

    rename presenter for needed care moment codes

--- app/presenters/plant_presenter.rb
+++ app/presenters/plant_presenter.rb
@@ -1,5 +1,5 @@
 class PlantPresenter < BasePresenter
-  def needed_care_moments
+  def needed_care_moment_codes
     moments = CareMoment::MOMENTS.keys.select do |code|
       code if __getobj__["#{code}_needed"] == 1
     end
